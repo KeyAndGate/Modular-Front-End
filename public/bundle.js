d23ba1dcc1101a012994f4ce501b30a7c00f0b98
Whitman Johnson <whit.ross.j@gmail.com>
2018-01-30 12:51:39 -0500

    marker.js WIP

--- public/bundle.js
+++ public/bundle.js
@@ -69,7 +69,7 @@
 
 console.log('Inside index.js')
 
-
+const {marker} = __webpack_require__(2)
 const mapboxgl = __webpack_require__(1);
 
 mapboxgl.accessToken = "pk.eyJ1Ijoid2hpcmxleXQiLCJhIjoiY2pkMXZ6bTdtMjFnMzJxbjJwaWl2ank5MiJ9.YVXDcHrtqGb339y9aGmteQ";
@@ -81,12 +81,9 @@ const map = new mapboxgl.Map({
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
 
-const markerDomEl = document.createElement("div"); // Create a new, detached DIV
-markerDomEl.style.width = "32px";
-markerDomEl.style.height = "39px";
-markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+const fullstackMarker = marker('activity', [-74.009151, 40.705086]).addTo(map)
 
-const fullstackMarker= new mapboxgl.Marker(markerDomEl).setLngLat([-74.009151, 40.705086]).addTo(map);
+// const fullstackMarker= new mapboxgl.Marker(markerDomEl).setLngLat().addTo(map);
 
 // document.getElementById('map').
 
@@ -663,6 +660,35 @@ module.exports={"$version":8,"$root":{"version":{"required":true,"type":"enum","
 
 
 //# sourceMappingURL=mapbox-gl.js.map
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const mapboxgl = __webpack_require__(1);
+
+function marker(type, coords){
+    let icon = null
+    switch(type) {
+    case 'hotel':
+        icon = 'http://i.imgur.com/D9574Cu.png';
+        break;
+    case 'restaurant':
+        icon = 'http://i.imgur.com/cqR6pUI.png'
+        break;
+    default:
+        icon = 'http://i.imgur.com/WbMOfMl.png'
+    }
+    const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+    markerDomEl.style.width = "32px";
+    markerDomEl.style.height = "39px";
+    markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";// needs to vary
+    console.log('MARKER MADE')
+    return new mapboxgl.Marker(markerDomEl).setLngLat(coords)
+}
+
+module.exports = { marker }
+
 
 /***/ })
 /******/ ]);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 console.log('Inside index.js')
 
-
+const {marker} = require('./marker.js')
 const mapboxgl = require("mapbox-gl");
 
 mapboxgl.accessToken = "pk.eyJ1Ijoid2hpcmxleXQiLCJhIjoiY2pkMXZ6bTdtMjFnMzJxbjJwaWl2ank5MiJ9.YVXDcHrtqGb339y9aGmteQ";
@@ -12,12 +12,9 @@ const map = new mapboxgl.Map({
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
 
-const markerDomEl = document.createElement("div"); // Create a new, detached DIV
-markerDomEl.style.width = "32px";
-markerDomEl.style.height = "39px";
-markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+const fullstackMarker = marker('activity', [-74.009151, 40.705086]).addTo(map)
 
-const fullstackMarker= new mapboxgl.Marker(markerDomEl).setLngLat([-74.009151, 40.705086]).addTo(map);
+// const fullstackMarker= new mapboxgl.Marker(markerDomEl).setLngLat().addTo(map);
 
 // document.getElementById('map').
 
